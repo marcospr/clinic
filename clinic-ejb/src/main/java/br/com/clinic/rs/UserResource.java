a86eda3e58e9082c15d0542f2deb0d8b7f46051a
Marcos Rocha <marcospr@cpqd.com.br>
2020-01-10 17:42:06 -0300

    alteracao resource de user

--- clinic-ejb/src/main/java/br/com/clinic/rs/UserResource.java
+++ clinic-ejb/src/main/java/br/com/clinic/rs/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.clinic.rs;
 
 import java.net.URI;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -13,8 +14,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.clinic.model.security.Profile;
 import br.com.clinic.model.security.UserSystem;
@@ -34,9 +37,13 @@ public class UserResource {
 	@GET
 	@Path("{id}")
 	@Produces({ "application/json" })
-	public UserSystem busca(@PathParam("id") long id) {
+	public Response busca(@PathParam("id") long id) {
 		UserSystem user = userService.findById(id);
-		return user;
+		if (user != null) {
+			return Response.ok(user).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
 	}
 
 	@POST
@@ -63,5 +70,18 @@ public class UserResource {
 	public Response altera(UserSystem user) {
 		userService.save(user);
 		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response listar() {
+
+		List<UserSystem> users = userService.findAll();
+
+		GenericEntity<List<UserSystem>> entity = new GenericEntity<List<UserSystem>>(users) {
+		};
+		return Response.status(Response.Status.OK).entity(entity).build();
+
 	}
 }
